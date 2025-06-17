a482089459a4680709f9cf317264edbe4ce6b501
Daniel <danieldejonge@t-online.de>
2025-06-17 13:07:11 +0200

    refactor: rl experiment

--- rl_intro/simulation/experiment.py
+++ rl_intro/simulation/experiment.py
@@ -5,14 +5,16 @@ from dataclasses import dataclass
 from typing import Optional
 from tqdm import trange
 import json
+from rl_intro.utils.logger import logger
+from rl_intro.utils.visualize import grid_str
 
 
 @dataclass
 class ExperimentLog:
     episode: int
     step: int
-    state: State
     action: Action
+    state: State
     reward: Reward
     terminal: Terminal
 
@@ -28,46 +30,35 @@ class Experiment:
         self.agent = agent
         self.env = env
         self.config = config
-        self.episodes = 0
-        self.steps = 0
-        self.last_action: Action = self.start_episode()
 
-    def start_episode(self) -> Action:
-        self.env.reset()
-        self.last_action = self.agent.start(self.env.state)
-        return self.last_action
-
-    def step(self) -> ExperimentLog:
-        self.steps += 1
-        state, reward, terminal = self.env.step(self.last_action)
-        action = self.agent.step(state, reward, terminal)
-        self.last_action = action
-        log = ExperimentLog(
-            episode=self.episodes,
-            step=self.steps,
-            state=State(state),
-            action=Action(action),
-            reward=Reward(reward),
-            terminal=Terminal(terminal),
-        )
-        if terminal:
-            print(
-                f"JUHU, reached terminal with reward {reward} in episode {self.episodes} at step {self.steps}."
-            )
-            self.episodes += 1
-            self.steps = 0
-            self.start_episode()
-
-        if self.steps > self.config.max_steps:
-            raise ValueError("Agent took too many steps, please check")
-        return log
+    def run_episodes(self, n_episodes: int, max_steps: int) -> list[ExperimentLog]:
+        logs = []
+        for episode in trange(n_episodes, desc="Episodes"):
+            self.env.reset()
+            action = self.agent.start(self.env.state)
+            for step in range(1, max_steps + 1):
+                state, reward, terminal = self.env.step(action)
+                logs.append(
+                    ExperimentLog(
+                        episode=episode,
+                        step=step,
+                        action=Action(action),
+                        state=State(state),
+                        reward=Reward(reward),
+                        terminal=Terminal(terminal),
+                    )
+                )
+                action = self.agent.step(state, reward, terminal)
+                if terminal:
+                    break
+            else:
+                logger.warning(
+                    f"Agent took too many steps ({self.config.max_steps}) in episode {episode}, resetting."
+                )
+        return logs
 
     def run(self) -> list[ExperimentLog]:
-        logs = []
-        for _ in trange(self.config.n_episodes, desc="Episodes"):
-            log = self.step()
-            logs.append(log)
-        return logs
+        return self.run_episodes(self.config.n_episodes, self.config.max_steps)
 
 
 if __name__ == "__main__":
@@ -76,33 +67,46 @@ if __name__ == "__main__":
     from rl_intro.environment.gridworld import GridWorld, GridWorldConfig
     from rl_intro.agent.policy import EpsilonGreedyPolicy, EpsilonGreedyConfig
 
+    w, h = 10, 4
+
     env_config = GridWorldConfig(
-        width=5,
-        height=5,
-        start_states=[1],
-        terminal_states=[24],
-        cliff_states=[20, 21],
-        wall_states=[6, 7, 8, 9],
+        width=w,
+        height=h,
+        start_states=[0],
+        terminal_states=[9],
+        cliff_states=[1, 2, 3, 4, 5, 6, 7, 8],
+        wall_states=[],
         random_seed=42,
     )
     env = GridWorld(env_config)
-    env.print()
 
     agent_config = AgentSarsaConfig(
-        n_states=env_config.width * env_config.height,
-        n_actions=4,
+        n_states=w * h,
+        n_actions=len(env.action_space),
         random_seed=42,
-        learning_rate=0.1,
-        discount=0.9,
+        learning_rate=0.3,
+        discount=1.0,
     )
     agent = AgentSarsa(
         agent_config, EpsilonGreedyPolicy(EpsilonGreedyConfig(epsilon=0.1))
     )
-    experiment_config = ExperimentConfig(n_episodes=5000, max_steps=200)
+    experiment_config = ExperimentConfig(n_episodes=2000, max_steps=200)
     experiment = Experiment(agent, env, experiment_config)
-    logs = experiment.run()
-    # save the logs as a json file
 
+    logger.info(f"Agent: {agent}")
+    logger.debug(env.to_str())
+    logger.debug(agent.policy.get_distribution(agent))
+    logger.debug(grid_str(agent.get_greedy_values(), w, h))
+
+    # ! running the experiment
+    logs = experiment.run()
+
+    logger.debug(agent.policy.get_distribution(agent))
+    logger.debug(grid_str(agent.get_greedy_actions(), w, h))
+    logger.debug(grid_str(agent.get_greedy_values(), w, h))
+    logger.debug(env.to_str())
+
+    # save the logs as a json file
     with open("experiment_logs.json", "w") as f:
         json.dump([log.__dict__ for log in logs], f, indent=4)
-    print("Experiment completed and logs saved to 'experiment_logs.json'.")
+    logger.info("Experiment completed and logs saved to 'experiment_logs.json'.")
