c25b68e0c08cee294ca409420abc9e151878c208
Daniel <danieldejonge@t-online.de>
2025-06-18 12:08:45 +0200

    add: data parsing and transform

---
+++ rl_intro/evaluation/data_handling.py
@@ -0,0 +1,65 @@
+from rl_intro.simulation.experiment import ExperimentLog, StepLog, ExperimentConfig
+from rl_intro.utils.logger import logger
+import json
+from pathlib import Path
+import pandas as pd
+
+
+def to_dataframe(experiment_log: ExperimentLog) -> pd.DataFrame:
+    rows = []
+    for step in experiment_log.steps:
+        row = {
+            "agent": experiment_log.agent,
+            "env": experiment_log.env,
+            "episode": step.episode,
+            "step": step.step,
+            "action": step.action,
+            "state": step.state,
+            "reward": step.reward,
+            "terminal": step.terminal,
+        }
+        rows.append(row)
+    return pd.DataFrame(rows)
+
+
+def to_dataframe_batch(experiment_logs: list[ExperimentLog]) -> pd.DataFrame:
+    return pd.concat([to_dataframe(log) for log in experiment_logs], ignore_index=True)
+
+
+def parse_experiment_data(data: dict) -> ExperimentLog:
+    # TODO: maybe use a lib for parsing dataclasses recursively
+    logs = [StepLog(**step) for step in data["steps"]]
+    exp_config = ExperimentConfig(**data["experiment_config"])
+    return ExperimentLog(
+        agent=data["agent"],
+        env=data["env"],
+        experiment_config=exp_config,
+        steps=logs,
+    )
+
+
+def parse_experiment_json(json_file: Path) -> ExperimentLog:
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    return parse_experiment_data(data)
+
+
+def parse_experiment_batch_json(json_file: Path) -> list[ExperimentLog]:
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    return [parse_experiment_data(exp) for exp in data]
+
+
+# * Example usage
+if __name__ == "__main__":
+    file_path = Path("experiment_logs.json")
+    experiment_log = parse_experiment_json(file_path)
+
+    df = to_dataframe(experiment_log)
+    logger.debug(df.head())
+
+    file_path = Path("experiment_batch_logs.json")
+    batch_experiment_logs = parse_experiment_batch_json(file_path)
+    logger.debug(batch_experiment_logs[0].agent)
+    df_batch = to_dataframe_batch(batch_experiment_logs)
+    logger.debug(df_batch.head())
