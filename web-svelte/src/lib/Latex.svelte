ee850e6b9657f725bbca5fe52b728053a2eb9d83
Daniel <danieldejonge@t-online.de>
2025-06-26 16:39:48 +0200

    add: latex component

---
+++ web-svelte/src/lib/Latex.svelte
@@ -0,0 +1,17 @@
+<script>
+	import katex from "katex";
+	let {math, displayMode} = $props()
+	
+	const options = {
+		displayMode: displayMode,
+		throwOnError: false
+	}
+	
+	let katexString = katex.renderToString(math, options);
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
+</svelte:head>
+
+{@html katexString}
